5cec7d8af52b7db91e875e4a2fb20a24b2dfcfc0
Nidumukkala <Harichandan.Nidumukkala@McKesson.com>
2021-01-19 18:21:13 -0500

    added new test which will fail

--- Test/DevOpsDemoTest/UnitTest1.cs
+++ Test/DevOpsDemoTest/UnitTest1.cs
@@ -8,12 +8,21 @@ namespace DevOpsDemoTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ThisWillPass()
         {
             var wfc = new WeatherForecastController();
             var wS = wfc.Get();
 
             Assert.Equal(5, wS.Count());
+        }
+
+        [Fact]
+        public void ThisWillFail()
+        {
+            var wfc = new WeatherForecastController();
+            var wS = wfc.Get();
+
+            Assert.Equal(2, wS.Count());
         }
     }
 }
