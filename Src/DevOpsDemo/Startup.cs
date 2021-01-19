d832c22732720a724d9b3dddcd2b526333209b41
Nidumukkala <Harichandan.Nidumukkala@McKesson.com>
2021-01-19 17:50:43 -0500

    removed http redirection

--- Src/DevOpsDemo/Startup.cs
+++ Src/DevOpsDemo/Startup.cs
@@ -44,7 +44,7 @@ namespace DevOpsDemo
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevOpsDemo v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
